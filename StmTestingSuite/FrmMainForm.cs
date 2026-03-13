fad0a875d60cca561f5faf2d5304478c9b0e1669
Patrick Nelson <supermariokart98@gmail.com>
2026-03-13 17:07:00 -0400

    Add inital Visual Studio files

---
+++ StmTestingSuite/FrmMainForm.cs
@@ -0,0 +1,15 @@
+namespace StmTestingSuite
+{
+    public partial class FrmMainForm : Form
+    {
+        public FrmMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
